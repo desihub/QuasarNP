9762f678fa1affbfbfcc57273407fab5d00c88b9
dylanagreen <dylan@mygreenfamily.us>
2021-02-26 19:37:23 -0800

    Add ability to load entire weights file to dict.

---
+++ quasarnp/io.py
@@ -0,0 +1,19 @@
+import h5py
+
+def load_file(filename):
+  result = {}
+
+  with h5py.File(filename, "r") as f:
+      m_weights = f['model_weights']
+
+      for k1, v1 in m_weights.items():
+          if len(v1) == 0: continue
+          a = v1[k1]
+
+          data_dict = {}
+          for k2, v2 in a.items():
+              data_dict[k2[:-2]] = v2[()]
+
+          result[k1] = data_dict
+
+  return result
