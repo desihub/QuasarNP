c2fb484862c82caaaf6172ae2517e07a6e9aef92
dylanagreen <dylan@mygreenfamily.us>
2021-03-02 19:07:53 -0800

    Add load_model and fix indentation errors.

--- quasarnp/io.py
+++ quasarnp/io.py
@@ -1,20 +1,26 @@
 import h5py
 
+from .model import QuasarNP
+
 def load_file(filename):
-  result = {}
+    result = {}
 
-  with h5py.File(filename, "r") as f:
-      m_weights = f['model_weights']
+    with h5py.File(filename, "r") as f:
+        m_weights = f['model_weights']
 
-      for k1, v1 in m_weights.items():
-          if len(v1) == 0: continue
-          a = v1[k1]
+        for k1, v1 in m_weights.items():
+            if len(v1) == 0: continue
+            a = v1[k1]
 
-          data_dict = {}
-          for k2, v2 in a.items():
-            # This :-2 strips off the :0 at the end of weights names.
-              data_dict[k2[:-2]] = v2[()]
+            data_dict = {}
+            for k2, v2 in a.items():
+              # This :-2 strips off the :0 at the end of weights names.
+                data_dict[k2[:-2]] = v2[()]
 
-          result[k1] = data_dict
+            result[k1] = data_dict
 
-  return result
+    return result
+
+def load_model(filename):
+    db = load_file(filename)
+    return QuasarNP(db)
