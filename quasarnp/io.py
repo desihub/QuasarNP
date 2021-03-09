964b04e8ea30250a31587ab98b3689910cd7ba15
dylanagreen <dylan@mygreenfamily.us>
2021-03-09 13:46:42 -0800

    Directly port read_truth and read_data from QuasarNet.

--- quasarnp/io.py
+++ quasarnp/io.py
@@ -1,3 +1,4 @@
+import fitsio
 import h5py
 import numpy as np
 
@@ -33,3 +34,195 @@ dll = 1e-3
 nbins = int((llmax-llmin)/dll)
 wave = 10**(llmin + np.arange(nbins)*dll)
 nmasked_max = len(wave)+1
+
+def read_truth(fi):
+    '''
+    reads a list of truth files and returns a truth dictionary
+    Arguments:
+        fi -- list of truth files (list of string)
+    Returns:
+        truth -- dictionary of THING_ID: metadata instance
+    '''
+
+    class metadata:
+        pass
+
+    cols = ['Z_VI','PLATE',
+            'MJD','FIBERID','CLASS_PERSON',
+            'Z_CONF_PERSON','BAL_FLAG_VI','BI_CIV']
+
+    truth = {}
+
+    for f in fi:
+        h = fitsio.FITS(f)
+        tids = h[1]['THING_ID'][:]
+        cols_dict = {c.lower():h[1][c][:] for c in cols}
+        h.close()
+        for i,t in enumerate(tids):
+            m = metadata()
+            for c in cols_dict:
+                setattr(m,c,cols_dict[c][i])
+            truth[t] = m
+
+    return truth
+
+def read_data(fi, truth=None, z_lim=2.1,
+        return_pmf=False, nspec=None):
+    '''
+    reads data from input file
+    Arguments:
+        fi -- list of data files (string iterable)
+        truth -- dictionary thind_id => metadata
+        z_lim -- hiz/loz cut (float)
+        return_pmf -- if True also return plate,mjd,fiberid
+        nspec -- read this many spectra
+    Returns:
+        tids -- list of thing_ids
+        X -- spectra reformatted to be fed to the network (numpy array)
+        Y -- truth vector (nqso, 5):
+                           STAR = (1,0,0,0,0), GAL = (0,1,0,0,0)
+                           QSO_LZ = (0,0,1,0,0), QSO_HZ = (0,0,0,1,0)
+                           BAD = (0,0,0,0,1)
+        z -- redshift (numpy array)
+        bal -- 1 if bal, 0 if not (numpy array)
+    '''
+
+    tids = []
+    X = []
+    Y = []
+    z = []
+    bal = []
+
+    if return_pmf:
+        plate = []
+        mjd = []
+        fid = []
+
+    for f in fi:
+        print('INFO: reading data from {}'.format(f))
+        h=fitsio.FITS(f)
+        if nspec is None:
+            nspec = h[1].get_nrows()
+        aux_tids = h[1]['TARGETID'][:nspec].astype(int)
+        ## remove thing_id == -1 or not in sdrq
+        w = (aux_tids != -1)
+        if truth is not None:
+            w_in_truth = np.in1d(aux_tids, list(truth.keys()))
+            print("INFO: removing {} spectra missing in truth".format((~w_in_truth).sum()),flush=True)
+            w &= w_in_truth
+        aux_tids = aux_tids[w]
+        aux_X = h[0][:nspec,:]
+
+        aux_X = aux_X[w]
+        if return_pmf:
+            aux_plate = h[1]['PLATE'][:][w]
+            aux_mjd = h[1]['MJD'][:][w]
+            aux_fid = h[1]['FIBERID'][:][w]
+            plate += list(aux_plate)
+            mjd += list(aux_mjd)
+            fid += list(aux_fid)
+
+        X.append(aux_X)
+        tids.append(aux_tids)
+
+        print("INFO: found {} spectra in file {}".format(aux_tids.shape, f))
+
+    tids = np.concatenate(tids)
+    X = np.concatenate(X)
+
+    if return_pmf:
+        plate = np.array(plate)
+        mjd = np.array(mjd)
+        fid = np.array(fid)
+
+    we = X[:,443:]
+    w = we.sum(axis=1)==0
+    print("INFO: removing {} spectra with zero weights".format(w.sum()))
+    X = X[~w]
+    tids = tids[~w]
+
+    if return_pmf:
+        plate = plate[~w]
+        mjd = mjd[~w]
+        fid = fid[~w]
+
+    mdata = np.average(X[:,:443], weights = X[:,443:], axis=1)
+    sdata = np.average((X[:,:443]-mdata[:,None])**2,
+            weights = X[:,443:], axis=1)
+    sdata=np.sqrt(sdata)
+
+    w = sdata == 0
+    print("INFO: removing {} spectra with zero flux".format(w.sum()))
+    X = X[~w]
+    tids = tids[~w]
+    mdata = mdata[~w]
+    sdata = sdata[~w]
+
+    if return_pmf:
+        plate = plate[~w]
+        mjd = mjd[~w]
+        fid = fid[~w]
+
+    X = X[:,:443]-mdata[:,None]
+    X /= sdata[:,None]
+
+    if truth==None:
+        if return_pmf:
+            return tids,X,plate,mjd,fid
+        else:
+            return tids,X
+
+    ## remove zconf == 0 (not inspected)
+    observed = [(truth[t].class_person>0) or (truth[t].z_conf_person>0) for t in tids]
+    observed = np.array(observed, dtype=bool)
+    tids = tids[observed]
+    X = X[observed]
+
+    if return_pmf:
+        plate = plate[observed]
+        mjd = mjd[observed]
+        fid = fid[observed]
+
+    ## fill redshifts
+    z = np.zeros(X.shape[0])
+    z[:] = [truth[t].z_vi for t in tids]
+
+    ## fill bal
+    bal = np.zeros(X.shape[0])
+    bal[:] = [(truth[t].bal_flag_vi*(truth[t].bi_civ>0))-\
+            (not truth[t].bal_flag_vi)*(truth[t].bi_civ==0) for t in tids]
+
+    ## fill classes
+    ## classes: 0 = STAR, 1=GALAXY, 2=QSO_LZ, 3=QSO_HZ, 4=BAD (zconf != 3)
+    nclasses = 5
+    sdrq_class = np.array([truth[t].class_person for t in tids])
+    z_conf = np.array([truth[t].z_conf_person for t in tids])
+
+    Y = np.zeros((X.shape[0],nclasses))
+    ## STAR
+    w = (sdrq_class==1) & (z_conf==3)
+    Y[w,0] = 1
+
+    ## GALAXY
+    w = (sdrq_class==4) & (z_conf==3)
+    Y[w,1] = 1
+
+    ## QSO_LZ
+    w = ((sdrq_class==3) | (sdrq_class==30)) & (z<z_lim) & (z_conf==3)
+    Y[w,2] = 1
+
+    ## QSO_HZ
+    w = ((sdrq_class==3) | (sdrq_class==30)) & (z>=z_lim) & (z_conf==3)
+    Y[w,3] = 1
+
+    ## BAD
+    w = z_conf != 3
+    Y[w,4] = 1
+
+    ## check that all spectra have exactly one classification
+    assert (Y.sum(axis=1).min()==1) and (Y.sum(axis=1).max()==1)
+
+    if return_pmf:
+        return tids,X,Y,z,bal,plate,mjd,fid
+
+    return tids,X,Y,z,bal
