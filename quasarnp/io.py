6fb67ae68c2e36eac60620c6de46ca2e1d7c8fd2
dylanagreen <dylan@mygreenfamily.us>
2021-03-18 16:53:03 -0700

    Refactor loading DESI exposures to take an array OR an int.
    - fibers now defines a boolean masking array of which fibers we want loaded.
    - Can alternatively pass an integer, in which case the first <n> fibers are loaded.

--- quasarnp/io.py
+++ quasarnp/io.py
@@ -248,7 +248,17 @@ def read_data(fi, truth=None, z_lim=2.1,
     return tids,X,Y,z,bal
 
 
-def load_desi_exposure(d, e, spec_number, nfibers=500):
+def load_desi_exposure(d, e, spec_number, fibers=np.ones(500, dtype="bool")):
+    # If you pass an int, load the first <fibers> fibers
+    if isinstance(fibers, int):
+        print("trig")
+        good_fibers = np.ones(fibers, dtype="bool")
+    else:
+        # In case you pass it as ints.
+        good_fibers = fibers.astype("bool")
+    assert len(good_fibers) == 500, "fibers input must include True/False for exactly 500 fibers."
+    assert 0 <= spec_number and spec_number <= 9, "spec_number must be between 0 and 9"
+
     # For now load cascades cframes files
     # Can/should be changed later.
     # TODO: Add support for loading by tile id + e rather than date + e
@@ -257,6 +267,7 @@ def load_desi_exposure(d, e, spec_number, nfibers=500):
 
     # Load each cam sequentially, then rebin and merge
     # We will be rebinning down to 443, which is the input size of QuasarNet
+    nfibers = np.sum(good_fibers)
     X_out = np.zeros((nfibers, 443))
 
     # ivar_out is the weights out, i.e. the ivar, we use this for normalization
@@ -267,8 +278,8 @@ def load_desi_exposure(d, e, spec_number, nfibers=500):
         h = fitsio.FITS(os.path.join(file_loc, f"cframe-{c.lower()}{spec_number}-{e}.fits"))
 
         # Load the flux and ivar
-        flux = h["FLUX"].read()[:nfibers, :]
-        ivar = h["IVAR"].read()[:nfibers, :]
+        flux = h["FLUX"].read()[good_fibers, :]
+        ivar = h["IVAR"].read()[good_fibers, :]
         w_grid = h["WAVELENGTH"].read()
 
         # Rebin the flux and ivar
