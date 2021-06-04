3cf4f85af9250fdbc5a4025bbc2caa403b661ad3
dylanagreen <dylan@mygreenfamily.us>
2021-06-04 15:56:06 -0700

    Add version string.

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
+import os, sys, glob, re
 from setuptools import setup, find_packages
 
-setup(
+def _get_version():
+    line = open('quasarnp/_version.py').readline().strip()
+    m = re.match("__version__\s*=\s*'(.*)'", line)
+    if m is None:
+        print('ERROR: Unable to parse version from: {}'.format(line))
+        version = 'unknown'
+    else:
+        version = m.groups()[0]
+
+    return version
+
+setup_keywords = dict(
     name='quasarnp',
-    version='0.1.0',
+    version=_get_version(),
     description='Numpy Implementation of QuasarNet',
     url='https://github.com/dylanagreen/QuasarNP',
     author='Dylan Green',
@@ -12,3 +24,5 @@ setup(
     install_requires=['numpy', 'fitsio', 'h5py'],
     zip_safe=False,
 )
+
+setup(**setup_keywords)
