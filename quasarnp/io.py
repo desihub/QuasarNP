c4c33a448214dbb50d3205b6412eeef5f7ea4f87
dylanagreen <dylanag@uci.edu>
2026-07-09 15:00:13 -0700

    Fix a hardcoded out grid length.

--- quasarnp/io.py
+++ quasarnp/io.py
@@ -408,7 +408,7 @@ def load_desi_exposure(dir_name, spec_number,
     # Load each cam sequentially, then rebin and merge
     # We will be rebinning down to 443, which is the input size of QuasarNet
     nfibers = np.sum(fibers > 0)
-    X_out = np.zeros((nfibers, 443))
+    X_out = np.zeros((nfibers, len(out_grid)))
 
     # ivar_out is the weights out, i.e. the ivar, we use this for normalization
     # Use zeros_like so we only have to change one
